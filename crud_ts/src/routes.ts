acc23278d78f702ca91d312484542a67c554e541
BUBUB <aluno@IPIRANGA.FATEC>
2024-09-04 20:20:46 -0300

    alterações nat

--- crud_ts/src/routes.ts
+++ crud_ts/src/routes.ts
@@ -4,7 +4,7 @@ const router = Router();
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '123456',
+  password: '',
   database: 'ts_crud'
 });
 connection.connect();
